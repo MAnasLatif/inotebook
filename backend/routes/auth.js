f1bb905842ae46964a8ecfbe3ff41cc9016adf09
Anas Coding <m.anaslatif.com@gmail.com>
2022-09-13 14:52:51 +0500

    video 49 completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,34 +2,52 @@ const express = require('express');
 const User = require('../models/User')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'anastestweb';
 
 router.post('/createuser', [
   body('email', 'Enter a Valid Email').isEmail(),
   body('name', 'Name minimum lenght is 5').isLength({ min: 3 }),
   body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
+
   // if there are errors, return Bad request and the error 
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  // check whether the user with this email exail exists already
 
   try {
 
+    // check whether the user with this email exail exists already
     let user = await User.findOne({ email: req.body.email });
-
     if (user) {
       return res.status(400).json({ error: "Sorry a user with this email already exists " })
     }
 
+    // Add password Encryption 
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt);
+
+    // create user 
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: secPass,
     })
-    res.json(user)
+
+    const data = {
+      user: {
+        id: user.id
+      }
+    }
+
+    const authtoken = jwt.sign(data, JWT_SECRET)
+    res.json({ authtoken })
+
   } catch (error) {
     res.status(500).send("Some Error Occured");
   }
